8327c4c85586d26fb60f4d39877793dacd5c1c77
Evgenii Vasiliev <evgenii.vasiliev@ax-team.com>
2025-03-11 07:51:07 +0300

    Sends a greeting as stream

--- src/main/java/com/example/grpc/demo/helloworld/GreeterImpl.java
+++ src/main/java/com/example/grpc/demo/helloworld/GreeterImpl.java
@@ -5,11 +5,13 @@ import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
 import io.grpc.stub.StreamObserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.grpc.server.service.GrpcService;
 
 @GrpcService
 public class GreeterImpl extends GreeterGrpc.GreeterImplBase{
-
+    private static final Logger LOG = LoggerFactory.getLogger(GreeterImpl.class);
     @Override
     public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
         HelloReply reply = HelloReply.newBuilder()
@@ -18,4 +20,24 @@ public class GreeterImpl extends GreeterGrpc.GreeterImplBase{
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
     }
+
+    @Override
+    public void streamHello(final HelloRequest request, final StreamObserver<HelloReply> responseObserver) {
+        LOG.info("Hello " + request.getName());
+        int count = 0;
+        while (count < 10) {
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello(" + count + ") " + request.getName()).build();
+            responseObserver.onNext(reply);
+            count++;
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                responseObserver.onError(e);
+                return;
+            }
+        }
+        responseObserver.onCompleted();
+    }
+
 }
